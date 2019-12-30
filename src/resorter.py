23bba6349a81f66fa133703e200c717a3ce24d21
Mikhail Petrov <smartptr@gmail.com>
2019-12-30 23:59:02 +0100

    Ask

--- src/resorter.py
+++ src/resorter.py
@@ -62,15 +62,22 @@ def compute(f, e):
 def resort(files, args):
     expr_list = args.expression.split(os.sep)
     for f in files:
-        logging.debug('resorting %s', f.path)
-        output = []
-        for e in expr_list:
-            value = []
-            for part in GROUP.split(e):
-                value.append(str(compute(f, part))
-                              if part.startswith('{') else part)
-            output.append(''.join(value))
-        yield (f.path, os.path.join(args.output or '', *output))
+        try:
+            logging.debug('resorting %s', f.path)
+            output = []
+            for e in expr_list:
+                value = []
+                for part in GROUP.split(e):
+                    value.append(str(compute(f, part))
+                                if part.startswith('{') else part)
+                output.append(''.join(value))
+            yield (f.path, os.path.join(args.output or '', *output))
+        except Exception as e:
+            logging.warning('Exception: %s', e)
+            if ask('Could not compute destination for {0}: {1}'.format(f.path, e),
+                {'Quit':'qQ', 'Ignore':'iI'}, 'i') in 'qQ': return False
+    return True
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='File organizer')
@@ -111,6 +118,17 @@ def parse_args():
 
     return parser.parse_args()
 
+def ask(msg, opts, default=None):
+    while True:
+        answer = input('{0} {1}? '.format(msg,'/'.join(opts.keys())))
+        if default is not None and len(answer) == 0:
+            return default
+        if len(answer) == 0: continue
+        for o in opts:
+            if answer in opts[o]:
+                logging.debug('answer: %s (%s)', answer, o)
+                return answer
+    
 
 def main(args):
     loglevel = logging.CRITICAL if args.silent else logging.WARNING
@@ -119,7 +137,7 @@ def main(args):
     if args.debug:
         loglevel = logging.DEBUG
     logging.basicConfig(
-        format='%(levelname)s:%(module)s.%(funcName)s:%(message)s', level=loglevel)
+        format='%(levelname)s:%(module)s.%(funcName)s: %(message)s', level=loglevel)
 
     modules.update(args)
 
@@ -164,8 +182,17 @@ def main(args):
 
     action = actions.ACTIONS['dry' if args.dry_run else args.action]
     for s, d in pairs:
-        action(s, d, args)
+        try:
+            action(s, d, args)
+        except Exception as e:
+            logging.warning('Exception: %s', e)
+            if ask('Could not {0} from {1} to {2}: {3}'
+                .format(args.action, s, d, e),
+                {'Quit': 'qQ', 'Ignore': 'iI'}, 'i') in 'qQ': break
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    try:
+        main(parse_args())
+    except KeyboardInterrupt as e:
+        logging.warning('Keyboard interrupt')
